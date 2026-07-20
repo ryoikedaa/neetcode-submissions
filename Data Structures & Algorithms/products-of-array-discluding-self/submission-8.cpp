7362af9bdde6c4268e4943809b9dd04f948bd4fd
Ryo Ikeda <ryoikeda@umich.edu>
2026-07-20 17:24:19 -0400

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> output(nums.size());
+
+        //forward'
+        int forwardProduct = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            output[i] = forwardProduct;
+            forwardProduct *= nums[i];
+        }
+
+        //backward
+        int backwardProduct = 1;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            output[i] *= backwardProduct;
+            backwardProduct *= nums[i];
+        }
+
+        return output;
+    }
+};
