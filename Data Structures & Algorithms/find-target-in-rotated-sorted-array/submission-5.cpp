d47cc52ed779f7ec78860eef0c37823935dadb41
Ryo Ikeda <ryoikeda@umich.edu>
2026-07-23 17:18:20 -0400

    Add: find-target-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-5.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            }
+
+
+            if (nums[left] <= nums[middle]){
+                if (target > nums[middle] || target < nums[left]) {
+                    left = middle + 1;
+                }
+                else {
+                    right = middle - 1;
+                }
+            }
+            else {
+                if (target < nums[middle] || target > nums[right]) {
+                    right = middle - 1;
+                }
+                else {
+                    left = middle + 1;
+                }
+            }
+
+
+        }
+        return -1;
+    }
+};
